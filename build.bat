1d82674bf194ed2c4ccf713307ef4a216a1be951
Jahnavi Nakirekanti <jahnavi.nakirekanti@cyberark.com>
2021-07-12 18:33:36 +0530

    CC-85944 -- Docker less environment, Moved all services into single service (UserManagement), It supports two flows. 1. API Based Flow 2. MFAWidget Flow controlled by flag

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
-START "angular build" cmd /c "cd angular && ng build"
-START "spring boot build" cmd /c "cd usermanagement && mvn clean install"
+START "angular build" cmd /c "cd angular && ng build && pause"
+START "spring boot build" cmd /c "cd usermanagement && mvn clean install && pause" 
