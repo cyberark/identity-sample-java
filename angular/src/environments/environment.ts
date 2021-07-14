442a6c2ff45a8bc5c007b1e9174143fdbde72d17
jnakirekanti <jahnavi.nakirekanti@cyberark.com>
2021-07-14 03:19:51 -0700

    CC-85944 -- Config file changes

--- angular/src/environments/environment.ts
+++ angular/src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
   baseUrl:"https://apidemo.cyberark.app:8080/",
   apiFqdn:"<YOUR_TENANT_FULLY_QUALIFIED_DOMAIN_NAME>",
   oauthAuthCodeFlowAppId: "<YOUR_OAUTH_APPLICATION_ID_FOR_AUTHCODE_FLOW>",
-  oauthAuthCodeFlowSco: "<YOUR_OAUTH_SCOPE_FOR_AUTHCODE_FLOW>",
+  oauthAuthCodeFlowScope: "<YOUR_OAUTH_SCOPE_FOR_AUTHCODE_FLOW>",
 };
 
 /*
