198c9fa45662b9d526f34cb93560bff4f3dd21a1
jnakirekanti <jahnavi.nakirekanti@cyberark.com>
2021-08-09 05:37:32 -0700

    CC-84762 - Space Issue fix  Changes to be committed: 	modified:   src/app/basiclogin/basiclogin.service.ts

--- angular/src/app/basiclogin/basiclogin.service.ts
+++ angular/src/app/basiclogin/basiclogin.service.ts
@@ -51,9 +51,7 @@ export class BasicLoginService {
     .set('Accept', 'application/json')
     .set('Authorization', 'Bearer ' + authentication_token);
 
-    let url = `https://${environment.apiFqdn}/oauth2/authorize/${environment.oauthAppId}?scope=
-      ${environment.oauthScope}&client_id=${clientId}&code_challenge=${codeChallenge}&code_challenge_method=S256
-      &response_type=code&redirect_uri=https://apidemo.cyberark.app:8080/RedirectResource`;
+    let url = `https://${environment.apiFqdn}/oauth2/authorize/${environment.oauthAppId}?scope=${environment.oauthScope}&client_id=${clientId}&code_challenge=${codeChallenge}&code_challenge_method=S256&response_type=code&redirect_uri=https://apidemo.cyberark.app:8080/RedirectResource`;
 
     return this.http.get<any>(url, { headers: head, withCredentials: true});
   }
