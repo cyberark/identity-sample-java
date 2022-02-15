ab797ebe189a8ddd1c1e763eeee1478fe816c6a8
Jahnavi Nakirekanti <jahnavi.nakirekanti@cyberark.com>
2022-02-15 07:38:27 -0800

    Integrated Signup with Java SDK methods

--- spring-boot/src/main/java/com/sampleapp/entity/User.java
+++ spring-boot/src/main/java/com/sampleapp/entity/User.java
@@ -38,38 +38,8 @@ public class User {
 	@JsonProperty("Password")
 	public char[] password;
 
-	@JsonProperty("HomeNumber")
-	private String homeNumber;
-
-	@JsonProperty("OfficeNumber")
-	private String officeNumber;
-
-	@JsonProperty("ForcePasswordChangeNext")
-	private boolean forcePasswordChangeNext;
-
 	@JsonProperty("MobileNumber")
 	private String mobileNumber;
-
-	@JsonProperty("Description")
-	private String description;
-
-	@JsonProperty("street_line_1")
-	private String streetLine1;
-
-	@JsonProperty("street_line_2")
-	private String streetLine2;
-
-	@JsonProperty("address_city")
-	private String city;
-
-	@JsonProperty("postal_code")
-	private String postalCode;
-
-	@JsonProperty("state_code")
-	private String state;
-
-	@JsonProperty("country_code")
-	private String country;
 
 	@JsonProperty("ReCaptchaToken")
 	public String ReCaptchaToken;
@@ -120,30 +90,6 @@ public class User {
 		this.password = password;
 	}
 
-	public String getHomeNumber() {
-		return homeNumber;
-	}
-
-	public void setHomeNumber(String homeNumber) {
-		this.homeNumber = homeNumber;
-	}
-
-	public String getOfficeNumber() {
-		return officeNumber;
-	}
-
-	public void setOfficeNumber(String officeNumber) {
-		this.officeNumber = officeNumber;
-	}
-
-	public boolean isForcePasswordChangeNext() {
-		return forcePasswordChangeNext;
-	}
-
-	public void setForcePasswordChangeNext(boolean forcePasswordChangeNext) {
-		this.forcePasswordChangeNext = forcePasswordChangeNext;
-	}
-
 	public String getMobileNumber() {
 		return mobileNumber;
 	}
@@ -152,69 +98,10 @@ public class User {
 		this.mobileNumber = mobileNumber;
 	}
 
-	public String getStreetLine1() {
-		return streetLine1;
-	}
-
-	public void setStreetLine1(String streetLine1) {
-		this.streetLine1 = streetLine1;
-	}
-
-	public String getStreetLine2() {
-		return streetLine2;
-	}
-
-	public void setStreetLine2(String streetLine2) {
-		this.streetLine2 = streetLine2;
-	}
-
-	public String getCity() {
-		return city;
-	}
-
-	public void setCity(String city) {
-		this.city = city;
-	}
-
-	public String getPostalCode() {
-		return postalCode;
-	}
-
-	public void setPostalCode(String postalCode) {
-		this.postalCode = postalCode;
-	}
-
-	public String getState() {
-		return state;
-	}
-
-	public void setState(String state) {
-		this.state = state;
-	}
-
-	public String getCountry() {
-		return country;
-	}
-
-	public void setCountry(String country) {
-		this.country = country;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
 	@Override
 	public String toString() {
 		return "User [displayName=" + displayName + ", mail=" + mail + ", name=" + name
-				+ ", password=" + password + ", homeNumber=" + homeNumber
-				+ ", officeNumber=" + officeNumber + ", forcePasswordChangeNext="
-				+ forcePasswordChangeNext + ", mobileNumber=" + mobileNumber
-				+ ", description=" + description + "]";
+				+ ", password=" + password + ", mobileNumber=" + mobileNumber + "]";
 	}
 
 	@JsonIgnore
